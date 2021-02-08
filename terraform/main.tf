a5a9984461982135c9c0489fcbb3e99b8e975b72
mohammad-gitup <mohammadsyed.kc@gmail.com>
2021-02-08 14:58:32 -0600

    Okay great got docker working, now time for Celo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "celo-303002"
-  credentials = "./credentials/Celo-85fd921ccb33.json"
+  credentials = "../credentials/Celo-85fd921ccb33.json"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
@@ -10,25 +10,6 @@ resource "google_compute_instance" "vm_instance" {
   machine_type = "f1-micro"
   tags =        ["celo-vm-instance"]
 
-
-  provisioner "remote-exec" {
-    connection { 
-        type    = "ssh"
-        user    = "ansible"
-        timeout = "500s"
-        private_key = file("./credentials/ansible")
-        host = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
-    }
-
-    inline = [
-        "sudo apt -y update",
-        "sudo apt -y install git",
-        "echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list",
-        "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367",
-        "sudo apt -y install ansible"
-    ]
-  }
-
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
@@ -36,7 +17,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   metadata = {
-    ssh-keys = "ansible:${file("./credentials/ansible.pub")}"
+    ssh-keys = "ansible:${file("../credentials/ansible.pub")}"
   }
 
   network_interface {
